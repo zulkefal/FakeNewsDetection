c273f3ea0b34afa53e3d0e399b469f2bd0339e06
Zulkefal <zulkefal.mohammadzai705@gmail.com>
2026-03-15 15:52:55 +0500

    Added a hyperparameter tuning using GridSearchCV for Logistic Regression

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from model import build_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -25,9 +26,25 @@ vectorizer, model = build_model()
 X_train_vec = vectorizer.fit_transform(X_train)
 X_test_vec = vectorizer.transform(X_test)
 
-model.fit(X_train_vec, y_train)
+param_grid = {
+    "C": [0.01, 0.1, 1, 10]
+}
 
-preds = model.predict(X_test_vec)
+grid = GridSearchCV(
+    model,
+    param_grid,
+    cv=3,
+    scoring="f1",
+    verbose=1
+)
+
+grid.fit(X_train_vec, y_train)
+
+best_model = grid.best_estimator_
+
+print("Best parameter:", grid.best_params_)
+
+preds = best_model.predict(X_test_vec)
 
 print(classification_report(y_test, preds))
 
