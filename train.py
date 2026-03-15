c0546365427a54dd98f340336be88f2b7b89c9fb
Zulkefal <zulkefal.mohammadzai705@gmail.com>
2026-03-15 15:48:05 +0500

    Added confusion matrix visualization for error analysis

--- train.py
+++ train.py
@@ -2,6 +2,9 @@ from preprocess import load_data
 from model import build_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 X, y = load_data()
 
@@ -19,3 +22,17 @@ model.fit(X_train_vec, y_train)
 preds = model.predict(X_test_vec)
 
 print(classification_report(y_test, preds))
+
+cm = confusion_matrix(y_test, preds)
+
+plt.figure(figsize=(6,4))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues",
+            xticklabels=["Fake", "Real"],
+            yticklabels=["Fake", "Real"])
+
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+plt.title("Confusion Matrix")
+
+plt.savefig("confusion_matrix.png")
+plt.show()
